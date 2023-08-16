c7d9704efc975b474a89b258352d4aaca7b228c0
Fahad Amin <fahad.amin@acetsolutions.com>
2023-08-16 15:11:13 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Facebook-Scraper',
+    version='2.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Fahadamin397',
+    author_email='',
+    description=''
+)
